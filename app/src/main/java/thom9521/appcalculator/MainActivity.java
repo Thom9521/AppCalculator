30bb61814b45855674d49fe1fbd34b8c0834bd88
Thom9521 <Thom9521@edu.easj.dk>
2018-05-02 11:23:35 +0200

    Added stuff

--- app/src/main/java/thom9521/appcalculator/MainActivity.java
+++ app/src/main/java/thom9521/appcalculator/MainActivity.java
@@ -24,7 +24,52 @@ public class MainActivity extends AppCompatActivity {
 
                 int num1 = Integer.parseInt(editText1.getText().toString());
                 int num2 = Integer.parseInt(editText2.getText().toString());
-                int result = num1 + num2;
+                double result = num1 + num2;
+                resultView.setText(result + ""); //"" er nødvendigt for at appen ikke crasher
+            }
+        });
+
+        Button minusBtn = (Button) findViewById(R.id.minusBtn);
+        minusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText1 = (EditText) findViewById(R.id.editText1);
+                EditText editText2 = (EditText) findViewById(R.id.editText2);
+                TextView resultView = (TextView) findViewById(R.id.resultTextView);
+
+                int num1 = Integer.parseInt(editText1.getText().toString());
+                int num2 = Integer.parseInt(editText2.getText().toString());
+                double result = num1 - num2;
+                resultView.setText(result + ""); //"" er nødvendigt for at appen ikke crasher
+            }
+        });
+
+        Button multiBtn = (Button) findViewById(R.id.mutliBtn);
+        multiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText1 = (EditText) findViewById(R.id.editText1);
+                EditText editText2 = (EditText) findViewById(R.id.editText2);
+                TextView resultView = (TextView) findViewById(R.id.resultTextView);
+
+                int num1 = Integer.parseInt(editText1.getText().toString());
+                int num2 = Integer.parseInt(editText2.getText().toString());
+                double result = num1 * num2;
+                resultView.setText(result + ""); //"" er nødvendigt for at appen ikke crasher
+            }
+        });
+
+        Button divBtn = (Button) findViewById(R.id.divBtn);
+        divBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText1 = (EditText) findViewById(R.id.editText1);
+                EditText editText2 = (EditText) findViewById(R.id.editText2);
+                TextView resultView = (TextView) findViewById(R.id.resultTextView);
+
+                double num1 = Integer.parseInt(editText1.getText().toString());
+                double num2 = Integer.parseInt(editText2.getText().toString());
+                double result = num1 / num2;
                 resultView.setText(result + ""); //"" er nødvendigt for at appen ikke crasher
             }
         });
